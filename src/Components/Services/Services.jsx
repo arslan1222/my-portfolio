49fee70f4db7f3215acd9814e369e2311365a45c
Arslan Haider <arslanhaidermughal8@gmail.com>
2025-01-07 15:37:31 +0500

    Truncate services content

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,40 +1,52 @@
-import React from 'react'
-import './Services.css'
-import bgIcon from '../../assets/themeTitle.png'
-import arrowIcon from '../../assets/arrow_icon.png'
-import { servicesData }  from '../../assets/services.js'
+import React, { useState } from "react";
+import "./Services.css";
+import bgIcon from "../../assets/themeTitle.png";
+import arrowIcon from "../../assets/arrow_icon.png";
+import { servicesData } from "../../assets/services.js";
 import { motion } from "framer-motion";
 
-
 const Services = () => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleExpand = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   return (
-    <motion.div id='services' className='services' 
-    
-    initial={{opacity: 0, x: -200}}
-        transition={{duration: 1}}
-        whileInView={{opacity: 1, x: 0}}
-        viewport={{once: true}}
+    <motion.div
+      id="services"
+      className="services"
+      initial={{ opacity: 0, x: -200 }}
+      transition={{ duration: 1 }}
+      whileInView={{ opacity: 1, x: 0 }}
+      viewport={{ once: true }}
     >
-        <div className="services-title">
-            <h1>My Services</h1>
-            <img src={bgIcon} alt="" />
-        </div>
-        <div className="services-container">
-            {servicesData.map((service, index)=>{
-                return <div key={index} className="format">
-                    <h3>{service.sNo}</h3>
-                    <h2>{service.sName}</h2>
-                    <p>{service.sDescription}</p>
-                    <div className="readmore">
-                        <p>Read More</p>
-                        <img src={arrowIcon} alt="" />
-                    </div>
-                </div>
-            })}
-        </div>
-
+      <div className="services-title">
+        <h1>My Services</h1>
+        <img src={bgIcon} alt="" />
+      </div>
+      <div className="services-container">
+        {servicesData.map((service, index) => {
+          return (
+            <div key={index} className="format">
+              <h3>{service.sNo}</h3>
+              <h2>{service.sName}</h2>
+              <p
+                className={isExpanded ? "expand" : "truncate"}
+                onClick={toggleExpand}
+              >
+                {service.sDescription}
+              </p>
+              <div className="readmore">
+                <p>Read More</p>
+                <img src={arrowIcon} alt="" />
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </motion.div>
-  )
-}
+  );
+};
 
-export default Services
+export default Services;
